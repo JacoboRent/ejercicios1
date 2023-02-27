d6827c820a736305a29e154881743e8e56724074
Jacobo Ren <rjacobo361@gmail.com>
2023-02-27 14:58:45 -0600

    agregado tabla de verdad 2

--- 2/verdad/index.php
+++ 2/verdad/index.php
@@ -3,18 +3,20 @@
 <head>
 	<title>Tabla de verdad</title>
 	<meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
 
+<div class = "tabla">
 <?php
 // definir el predicado lógico
 function predicado_logico($p, $q, $r) {
     return ($p && $q) || (!$q && $r);
 }
 
-// función para imprimir la tabla de verdad
+// función para imprimir la tabla de verdad 
 function tabla_de_verdad($predicado) {
-    echo "<table>";
+    echo "<table> <br>";
     echo "<tr><th>p</th><th>q</th><th>r</th><th>resultado</th></tr>";
     $valores_booleanos = array(true, false);
     foreach ($valores_booleanos as $p) {
@@ -32,6 +34,7 @@ function tabla_de_verdad($predicado) {
 tabla_de_verdad("predicado_logico");
 
 ?>
+</div>
 
 </body>
 </html>
